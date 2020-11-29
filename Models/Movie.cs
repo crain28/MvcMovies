e4ebfcdad7818bd76b8025783ed02563990078e3
Cole Crain <54562876+crain28@users.noreply.github.com>
2020-11-28 19:21:16 -0500

    Add files via upload
    files because I had issue in visual studio

---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+    }
+}
